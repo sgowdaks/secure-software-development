8f1c1a1204da77355619ac73c93793c3be817c12
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2022-04-30 21:23:45 -0700

    Add files via upload

---
+++ flask-development/adding_to_db.py
@@ -0,0 +1,11 @@
+import sqlite3
+from passlib.hash import pbkdf2_sha256
+
+def add_new_details(userId, name, email, password):
+    con = sqlite3.connect('user_details.db')
+    cur = con.cursor()
+    cur.execute(
+        "INSERT INTO users VALUES (?, ?, ?, ?)",
+        (userId, name, email, pbkdf2_sha256.hash(password)))
+    con.commit()
+    con.close()
